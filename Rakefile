38d55146cec93ab5ddf3fb0b6f376200012a26dc
Luiza Melo <meloluiza@live.com>
2019-09-09 23:30:05 -0300

    congratulate.rb

---
+++ Rakefile
@@ -0,0 +1,10 @@
+task :default do
+  puts '🤖 Saudações humano. Eu sou o bot de aniversário'
+  puts 'Minhas tarefas são: Dar parabéns!'
+end
+
+desc 'Checks if today is someone\'s birthday. If so, pushes a message to Slack.'
+task congratulate: :dotenv do
+  ruby 'congratulate.rb'
+end
+
---
+++ congratulate.rb
@@ -0,0 +1,24 @@
+APP_ROOT = File.dirname(__FILE__)
+
+$:.unshift(File.join(APP_ROOT, 'lib'))
+require 'birthday_bot'
+require 'env_reader'
+require 'dotenv/load'
+require 'pry-byebug'
+
+puts '🤖 Bot is booting...'
+
+configs = EnvReader.new()
+abort '❌ Bot is shutting down' if not configs.load
+
+puts 'Reading configurations'
+
+url = configs.slack_url
+channel = configs.channel_name
+greeting = configs.greeting_msg
+name = configs.bot_name
+emoji = configs.bot_emoji
+
+puts '🤖 Bot is activating...'
+bot = SlackBot.new(url, channel, greeting, name, emoji)
+bot.launch!
